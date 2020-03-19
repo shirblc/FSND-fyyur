2664213b6bd88006da8733ddf88979c0c650deab
Shir Bar Lev <shiruchii@gmail.com>
2020-03-19 12:32:58 +0200

    Added missing attributes to Artist model.

--- app.py
+++ app.py
@@ -56,9 +56,10 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(240))
     shows = db.relationship('Show', backref='show_artist')
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Show(db.Model):
     __tablename__ = 'shows'
