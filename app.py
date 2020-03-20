9ec4121aa106dfa533a5f1a82c5e5cd724304832
Shir Bar Lev <shiruchii@gmail.com>
2020-03-20 15:01:33 +0200

    Reset 'genres' column to change the column type.

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.PickleType)
+    genres = db.Column(db.String(240))
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(240))
