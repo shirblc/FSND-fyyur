df01f22ec5e517c8b3e1e29081e54e4ed80fc928
Shir Bar Lev <shiruchii@gmail.com>
2020-03-20 16:31:44 +0200

    Deleted duplicate field.

--- forms.py
+++ forms.py
@@ -87,9 +87,6 @@ class VenueForm(Form):
     phone = StringField(
         'phone'
     )
-    image_link = StringField(
-        'image_link'
-    )
     genres = SelectMultipleField(
         # TODO implement enum restriction
         'genres', validators=[DataRequired()],
